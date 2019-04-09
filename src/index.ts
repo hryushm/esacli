9cd03443262cc1997f7f0fb08a17389e4fe876a4
hryushm <hryushm@gmail.com>
2019-04-10 00:03:06 +0900

    Add argument parser

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello");
+import program from "commander";
+
+program
+  .version("0.1.0")
+  .option("-q, --query [querystring]", "query string")
+  .parse(process.argv);
+
+console.log(`query parameter is ${program.query}`);
